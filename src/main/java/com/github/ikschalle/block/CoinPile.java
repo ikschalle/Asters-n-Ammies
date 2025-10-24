580e59e557826114b122850e82077b05bdd9762a
ikschalle <67116818+ikschalle@users.noreply.github.com>
2025-10-24 03:27:09 +0100

    Coinpile Progress!!!!!
    - Coinpile blocks can now increment states when using self
    - Coinpiles drop self proportional to COINS property
    - Collision boxes update when incrementing states
    - Added Asterette Coinpiles

--- src/main/java/com/github/ikschalle/block/CoinPile.java
+++ src/main/java/com/github/ikschalle/block/CoinPile.java
@@ -5,7 +5,9 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
@@ -14,6 +16,8 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -21,70 +25,85 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
+import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
 
 public class CoinPile extends Block implements SimpleWaterloggedBlock {
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
     public static final IntegerProperty COINS = IntegerProperty.create("coins", 1,16);
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
-    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
-    private static final Supplier<VoxelShape> voxelShapeSupplier = () -> {
-        VoxelShape shape = Shapes.empty();
-        shape = Shapes.join(shape, Shapes.box(0.515625, 0, 0.515625, 0.984375, 0.25, 0.984375), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.515625, 0.25, 0.515625, 0.984375, 0.5, 0.984375), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.515625, 0.5, 0.515625, 0.984375, 0.75, 0.984375), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.515625, 0.75, 0.515625, 0.984375, 1, 0.984375), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.515625, 0.75, 0.015625, 0.984375, 1, 0.484375), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.015625, 0.75, 0.015625, 0.484375, 1, 0.484375), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.015625, 0.75, 0.515625, 0.484375, 1, 0.984375), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.015625, 0.5, 0.015625, 0.484375, 0.75, 0.484375), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.515625, 0.5, 0.015625, 0.984375, 0.75, 0.484375), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.015625, 0.5, 0.515625, 0.484375, 0.75, 0.984375), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.515625, 0.25, 0.015625, 0.984375, 0.5, 0.484375), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.015625, 0.25, 0.015625, 0.484375, 0.5, 0.484375), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.015625, 0.25, 0.515625, 0.484375, 0.5, 0.984375), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.515625, 0, 0.015625, 0.984375, 0.25, 0.484375), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.015625, 0, 0.015625, 0.484375, 0.25, 0.484375), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.015625, 0, 0.515625, 0.484375, 0.25, 0.984375), BooleanOp.OR);
-
-        return shape;
+    private static final VoxelShape[] SHAPES = new VoxelShape[]{
+            Shapes.box(0.5D,0.01D,0.5D,1,0.25D,1D),
+            Shapes.box(0D,0.01D,0.5D,0.5D,0.25D,1D),
+            Shapes.box(0.5D,0.01D,0D,1D,0.25D,0.5D),
+            Shapes.box(0D,0.01D,0D,0.5D,0.25D,0.5D),
+            Shapes.box(0.5D,0.25D,0.5D,1D,0.5D,1D),
+            Shapes.box(0D,0.25D,0.5D,0.5D,0.5D,1D),
+            Shapes.box(0.5D,0.25D,0D,1D,0.5D,0.5D),
+            Shapes.box(0D,0.25D,0D,0.5D,0.5D,0.5D),
+            Shapes.box(0.5D,0.5D,0.5D,1D,0.75D,1D),
+            Shapes.box(0D,0.5D,0.5D,0.5D,0.75D,1D),
+            Shapes.box(0.5D,0.5D,0D,1D,0.75D,0.5D),
+            Shapes.box(0D,0.5D,0D,0.5D,0.75D,0.5D),
+            Shapes.box(0.5D,0.75D,0.5D,1D,1D,1D),
+            Shapes.box(0D,0.75D,0.5D,0.5D,1D,1D),
+            Shapes.box(0.5D,0.75D,0D,1D,1D,0.5D),
+            Shapes.box(0D,0.75D,0D,0.5D,1D,0.5D)
     };
-
-
-    public static final Map<Direction, VoxelShape> SHAPE = net.minecraft.Util.make(new HashMap<>(), map -> {
-        for (Direction direction : Direction.Plane.HORIZONTAL) {
-            map.put(direction, GeneralUtil.rotateShape(Direction.NORTH, direction, voxelShapeSupplier.get()));
-        }
-    });
-
 
     public CoinPile(Properties properties){
         super(properties);
         this.registerDefaultState(this.defaultBlockState()
-                .setValue(FACING, Direction.NORTH)
                 .setValue(COINS, 1)
                 .setValue(WATERLOGGED, Boolean.FALSE)
         );
     }
 
+    public @NotNull VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+        int coins = blockState.getValue(COINS)-1;
+        VoxelShape shape = Shapes.empty();
 
-    public @NotNull VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
-        return SHAPE.get(state.getValue(FACING));
+        for (int i = 0; i <= coins ; i++) {
+            shape = Shapes.join(shape, SHAPES[i], BooleanOp.OR);
+        }
+        return shape;
     }
 
-
     @Nullable
-    public BlockState getStateForPlacement(BlockPlaceContext context) {
+    public BlockState getStateForPlacement(BlockPlaceContext context) { // On place w/ context
         BlockState blockstate = context.getLevel().getBlockState(context.getClickedPos());
         if (blockstate.is(this)) {
             int i = blockstate.getValue(COINS);
-            return blockstate.setValue(COINS, Math.min(16, i + 1));
+            return blockstate.setValue(COINS, Math.min(i + 1, 16));
         } else {
             return super.getStateForPlacement(context);
         }
     }
 
+
+    public boolean canBeReplaced(@NotNull BlockState blockState, BlockPlaceContext blockPlaceContext) {
+        return !blockPlaceContext.isSecondaryUseActive() && blockPlaceContext.getItemInHand().is(this.asItem()) && blockState.getValue(COINS) < 17 || super.canBeReplaced(blockState, blockPlaceContext);
+    }
+
+
+    public BlockState updateShape(BlockState blockState, Direction direction, BlockState state, LevelAccessor levelAccessor, BlockPos blockPos, BlockPos blockPos1) {
+        if (!blockState.canSurvive(levelAccessor, blockPos)) {
+            return Blocks.AIR.defaultBlockState();
+        } else {
+            if (blockState.getValue(WATERLOGGED)) {
+                levelAccessor.scheduleTick(blockPos, Fluids.WATER, Fluids.WATER.getTickDelay(levelAccessor));
+            }
+
+            return super.updateShape(blockState, direction, state, levelAccessor, blockPos, blockPos1);
+        }
+    }
+
+
+    public FluidState getFluidState(BlockState blockState) {
+        return blockState.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(blockState);
+    }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
--- src/main/java/com/github/ikschalle/datagen/loot/ModBlockLootTables.java
+++ src/main/java/com/github/ikschalle/datagen/loot/ModBlockLootTables.java
@@ -1,15 +1,24 @@
 package com.github.ikschalle.datagen.loot;
 
 import com.github.ikschalle.Asters_n_ammies;
+import com.github.ikschalle.block.CoinPile;
+import net.minecraft.advancements.critereon.StatePropertiesPredicate;
 import net.minecraft.data.loot.BlockLootSubProvider;
-import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.flag.FeatureFlags;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.PinkPetalsBlock;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraftforge.registries.RegistryObject;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Set;
+import java.util.stream.IntStream;
 
 public class ModBlockLootTables extends BlockLootSubProvider {
     public ModBlockLootTables() {
@@ -18,12 +27,29 @@ public class ModBlockLootTables extends BlockLootSubProvider {
 
     @Override
     protected void generate() {
-        this.dropSelf(Asters_n_ammies.ASTER_BLOCK.get());
-        this.dropSelf(Asters_n_ammies.ASTERETTE_BLOCK.get());
+        this.createCoinPileDrops(Asters_n_ammies.ASTER_BLOCK.get());
+        this.createCoinPileDrops(Asters_n_ammies.ASTERETTE_BLOCK.get());
     }
 
     @Override
     protected @NotNull Iterable<Block> getKnownBlocks(){
          return Asters_n_ammies.BLOCKS.getEntries().stream().map(RegistryObject::get)::iterator;
     }
+
+    private void createCoinPileDrops(Block block) {
+        LootTable.Builder table = LootTable.lootTable();
+        LootPool.Builder pool = LootPool.lootPool().setRolls(ConstantValue.exactly(1));
+
+        for (int i = 1; i <= 16; i++) {
+            pool.add(LootItem.lootTableItem(block)
+                    .when(LootItemBlockStatePropertyCondition.hasBlockStateProperties(block)
+                            .setProperties(StatePropertiesPredicate.Builder.properties()
+                                    .hasProperty(CoinPile.COINS, i)))
+                    .apply(SetItemCountFunction.setCount(ConstantValue.exactly(i)))
+            );
+        }
+
+        add(block, table.withPool(pool));
+    }
 }
+
