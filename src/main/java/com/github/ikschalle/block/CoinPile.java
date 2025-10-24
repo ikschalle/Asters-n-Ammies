6bf0347d597f53d1d9597c2c45b35024aeb6bf1b
ikschalle <67116818+ikschalle@users.noreply.github.com>
2025-10-24 14:56:42 +0100

    Coinpile Random Y Rotation
    Adding all those rotations blockstates is expensive so i have opted to use random model Y rots for now. There are better ways to do this but this is my first foray
    
    - Coinpiles now place as waterlogged blocks if placed in water rather than placing unwaterlogged
    - Coinpile voxelshapes are calculated for every new layer rather than every new coin

--- src/main/java/com/github/ikschalle/block/CoinPile.java
+++ src/main/java/com/github/ikschalle/block/CoinPile.java
@@ -1,15 +1,11 @@
 package com.github.ikschalle.block;
 
-import com.github.ikschalle.util.GeneralUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SimpleWaterloggedBlock;
-import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -18,6 +14,7 @@ import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -25,60 +22,32 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
-import java.awt.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Supplier;
 
 public class CoinPile extends Block implements SimpleWaterloggedBlock {
-    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
-    public static final IntegerProperty COINS = IntegerProperty.create("coins", 1,16);
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
-    private static final VoxelShape[] SHAPES = new VoxelShape[]{
-            Shapes.box(0.5D,0.01D,0.5D,1,0.25D,1D),
-            Shapes.box(0D,0.01D,0.5D,0.5D,0.25D,1D),
-            Shapes.box(0.5D,0.01D,0D,1D,0.25D,0.5D),
-            Shapes.box(0D,0.01D,0D,0.5D,0.25D,0.5D),
-            Shapes.box(0.5D,0.25D,0.5D,1D,0.5D,1D),
-            Shapes.box(0D,0.25D,0.5D,0.5D,0.5D,1D),
-            Shapes.box(0.5D,0.25D,0D,1D,0.5D,0.5D),
-            Shapes.box(0D,0.25D,0D,0.5D,0.5D,0.5D),
-            Shapes.box(0.5D,0.5D,0.5D,1D,0.75D,1D),
-            Shapes.box(0D,0.5D,0.5D,0.5D,0.75D,1D),
-            Shapes.box(0.5D,0.5D,0D,1D,0.75D,0.5D),
-            Shapes.box(0D,0.5D,0D,0.5D,0.75D,0.5D),
-            Shapes.box(0.5D,0.75D,0.5D,1D,1D,1D),
-            Shapes.box(0D,0.75D,0.5D,0.5D,1D,1D),
-            Shapes.box(0.5D,0.75D,0D,1D,1D,0.5D),
-            Shapes.box(0D,0.75D,0D,0.5D,1D,0.5D)
-    };
+    public static final IntegerProperty COINS = IntegerProperty.create("coins", 1,16);
 
     public CoinPile(Properties properties){
         super(properties);
         this.registerDefaultState(this.defaultBlockState()
                 .setValue(COINS, 1)
-                .setValue(WATERLOGGED, Boolean.FALSE)
         );
     }
 
     public @NotNull VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
-        int coins = blockState.getValue(COINS)-1;
-        VoxelShape shape = Shapes.empty();
-
-        for (int i = 0; i <= coins ; i++) {
-            shape = Shapes.join(shape, SHAPES[i], BooleanOp.OR);
-        }
-        return shape;
+        double coin_layers = 0.25 * Math.floor((double) (blockState.getValue(COINS) + 3) / 4); // every layer is 4 coins, add more collision when new layer starts (terms in sequence 4n-3)
+        return Shapes.box(0.015625D, 0D, 0.015625D, 0.984375D, coin_layers, 0.984375D);
     }
 
     @Nullable
     public BlockState getStateForPlacement(BlockPlaceContext context) { // On place w/ context
         BlockState blockstate = context.getLevel().getBlockState(context.getClickedPos());
+        FluidState fluidstate = context.getLevel().getFluidState(context.getClickedPos());
         if (blockstate.is(this)) {
             int i = blockstate.getValue(COINS);
             return blockstate.setValue(COINS, Math.min(i + 1, 16));
         } else {
-            return super.getStateForPlacement(context);
+            return this.defaultBlockState().setValue(WATERLOGGED, fluidstate.getType() == Fluids.WATER);
         }
     }
 
@@ -105,9 +74,13 @@ public class CoinPile extends Block implements SimpleWaterloggedBlock {
         return blockState.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(blockState);
     }
 
+    public boolean isPathfindable(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, PathComputationType pathComputationType) {
+        return false;
+    }
+
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(COINS, WATERLOGGED, FACING);
+        builder.add(WATERLOGGED, COINS);
     }
 
 }
